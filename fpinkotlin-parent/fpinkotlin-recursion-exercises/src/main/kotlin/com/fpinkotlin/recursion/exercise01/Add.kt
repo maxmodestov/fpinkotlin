d7098c238b17e40d4a5a46159b0a2bee58938a14
Maxim Modestov <maks.modestov@gmail.com>
2019-10-06 20:10:11 +0200

    Recursion: ex01, ex02

--- fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise01/Add.kt
+++ fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise01/Add.kt
@@ -1,7 +1,13 @@
 package com.fpinkotlin.recursion.exercise01
 
 
-fun add(a: Int, b: Int): Int = TODO("add")
+fun addLong(a: Int, b: Int): Int {
+    tailrec fun addInternal(result: Int, counter: Int, addFinal: Int): Int =
+            if (counter == addFinal) result else addInternal(inc(result), inc(counter), addFinal)
+    return addInternal(a, 0, b)
+}
+
+tailrec fun add(a: Int, b: Int): Int = if (b == 0) a else add(inc(a), dec(b))
 
 fun inc(n: Int) = n + 1
 fun dec(n: Int) = n - 1
--- fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise02/Factorial.kt
+++ fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise02/Factorial.kt
@@ -1,5 +1,7 @@
 package com.fpinkotlin.recursion.exercise02
 
+typealias FactorialFun = (Int) -> Int
+
 object Factorial {
-    val factorial: (Int)-> Int = TODO("factorial")
+    val factorial: FactorialFun by lazy<FactorialFun> { { n -> if (n <= 1) n else n * factorial(n - 1) } }
 }
